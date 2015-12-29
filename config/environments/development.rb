880d1acd18e5edd3a838a0863d6e65467b28749e
Jackie Luo <jacquelineluo95@gmail.com>
2015-12-28 22:20:09 -0600

    Create navbar with Bootstrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,10 +14,11 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {:host => "localhost:3000"}
-  config.action_mailer.smtp_settings = {
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                  587,
     domain:               'gmail.com',
