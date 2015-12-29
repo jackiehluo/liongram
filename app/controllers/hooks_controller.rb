3269285886c0becfca3aff16a5dadb848c198117
Jackie Luo <jacquelineluo95@gmail.com>
2015-12-29 01:15:16 -0600

    Skip filter for requiring user

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,4 +1,5 @@
 class HooksController < ApplicationController
+  skip_before_filter :require_user
   skip_before_filter :verify_authenticity_token, :only => [:new_order_callback]
 
   def new_order_callback
