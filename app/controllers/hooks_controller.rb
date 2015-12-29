14790e5a41ec7b5208ba48e57f34faffa09b60a8
Jackie Luo <jacquelineluo95@gmail.com>
2015-12-29 00:50:57 -0600

    Send 200 response

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -2,7 +2,6 @@ class HooksController
   def new_order_callback
     puts params.inspect
     order = Order.new(:buyer_name => params[:Field1])
-    order.save!
-    render :nothing => true
+    head 200, content_type: "text/html"
   end
 end
