b2a1715e3754a30570e97ac9d6e21de09c782daf
Jackie Luo <jacquelineluo95@gmail.com>
2015-12-29 01:27:51 -0600

    Switch to full debug logging

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -3,7 +3,8 @@ class HooksController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:new_order_callback]
 
   def new_order_callback
-    order = Order.new(params)
+    order = Order.new(:buyer_name => params['Field1'])
+    order.save!
     head 200, content_type: "text/html"
   end
 end
