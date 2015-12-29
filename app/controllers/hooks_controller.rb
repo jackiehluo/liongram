7ad5b8b6df726c522e231aecc24c323dcfbee42c
Jackie Luo <jacquelineluo95@gmail.com>
2015-12-29 01:18:49 -0600

    Create orders with params

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -3,8 +3,7 @@ class HooksController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:new_order_callback]
 
   def new_order_callback
-    puts params.inspect
-    order = Order.new(:buyer_name => params[:Field1])
+    order = Order.new(params)
     head 200, content_type: "text/html"
   end
 end
